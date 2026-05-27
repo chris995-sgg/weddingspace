2f52d36a6fa7b1c8855d6eef6c955c7cf708b28c
Chris <chriskitscha@gmail.com>
2026-05-28 00:25:55 +0200

    stopzeit zwischen upload weg

--- app/upload/[id]/page.tsx
+++ app/upload/[id]/page.tsx
@@ -89,17 +89,17 @@ export default function UploadPage() {
     try {
       const CONCURRENT_UPLOADS = 4;
 
-      for (let i = 0; i < files.length; i += CONCURRENT_UPLOADS) {
-        const batch = files.slice(i, i + CONCURRENT_UPLOADS);
+      //for (let i = 0; i < files.length; i += CONCURRENT_UPLOADS) {
+        //const batch = files.slice(i, i + CONCURRENT_UPLOADS);
 
-        await Promise.all(
-          batch.map(async (file) => {
-            await uploadSingleFile(file);
-            setUploadedCount((prev) => prev + 1);
-            await new Promise((resolve) => setTimeout(resolve, 50));
-          })
-        );
-      }
+       // await Promise.all(
+        //  batch.map(async (file) => {
+          //  await uploadSingleFile(file);
+            //setUploadedCount((prev) => prev + 1);
+           // await new Promise((resolve) => setTimeout(resolve, 50));
+         // })
+       // );
+      //}
 
       alert("Fotos erfolgreich hochgeladen!");
       setFiles([]);
