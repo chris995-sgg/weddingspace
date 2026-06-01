96ea0b61f132d9e9d37ca5400935ba86d07fb143
Chris <chriskitscha@gmail.com>
2026-06-02 00:42:13 +0200

    design

--- app/gallery/[id]/page.tsx
+++ app/gallery/[id]/page.tsx
@@ -315,13 +315,13 @@ useEffect(() => {
           </Link>
         )}
 
-        <div className="mt-0 md:mt-10 bg-white/50 backdrop-blur rounded-[1.5rem] p-8 shadow-2xl border border-white/50">
+       <div className="bg-white/50 backdrop-blur rounded-[2rem] p-8 md:py-5 shadow-2xl border border-white/50">
           <div className="flex flex-col md:flex-row justify-between items-center gap-4">
           <div className="text-center">
-         <h1 className="text-3xl font-bold text-[#3b3128]">
+         <h1 className="text-2xl font-bold text-[#3b3128]">
          {eventTitle}
         </h1>
-        <p className="mt-2 text-2xl font-semibold text-[#6b6258]">
+        <p className="mt-2 text-2xl font-semibold text-[#3b3128]">
           Galerie
         </p>
         </div>
