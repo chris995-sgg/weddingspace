6fc53cc689833c7fc4e370929b77a76764333c34
Chris <chriskitscha@gmail.com>
2026-06-02 00:33:25 +0200

    design

--- app/gallery/[id]/page.tsx
+++ app/gallery/[id]/page.tsx
@@ -315,7 +315,7 @@ useEffect(() => {
           </Link>
         )}
 
-        <div className="mt-12 bg-white/50 backdrop-blur rounded-[1.5rem] p-8 shadow-2xl border border-white/50">
+        <div className="mt-0 md:mt-10 bg-white/50 backdrop-blur rounded-[1.5rem] p-8 shadow-2xl border border-white/50">
           <div className="flex flex-col md:flex-row justify-between items-center gap-4">
           <div className="text-center">
          <h1 className="text-3xl font-bold text-[#3b3128]">
