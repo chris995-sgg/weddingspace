59249ad42087996d02267836901e4722038f3a54
Chris <chriskitscha@gmail.com>
2026-06-05 01:43:37 +0200

    q

--- app/gallery/[id]/page.tsx
+++ app/gallery/[id]/page.tsx
@@ -39,10 +39,10 @@ type ImageLoadReport = {
   attemptReports: ImageAttemptReport[];
 };
 
-const CONCURRENT_LOADS = 6;
+const CONCURRENT_LOADS = 7;
 
-const PRELOAD_ATTEMPTS = 10;
-const PRELOAD_TIMEOUT_MS = 1000;
+const PRELOAD_ATTEMPTS = 20;
+const PRELOAD_TIMEOUT_MS = 1500;
 const PRELOAD_RETRY_DELAY_MS = 50;
 
 const VISIBLE_IMG_RETRIES = 5;
