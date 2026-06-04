1351cf932ec6b26ed6699d30c7a55cb683c0556e
Chris <chriskitscha@gmail.com>
2026-06-04 22:15:02 +0200

    3

--- app/gallery/[id]/page.tsx
+++ app/gallery/[id]/page.tsx
@@ -39,9 +39,9 @@ type ImageLoadReport = {
   attemptReports: ImageAttemptReport[];
 };
 
-const BATCH_SIZE = 4;
-const PRELOAD_ATTEMPTS = 10;
-const PRELOAD_TIMEOUT_MS = 1500;
+const BATCH_SIZE = 8;
+const PRELOAD_ATTEMPTS = 3;
+const PRELOAD_TIMEOUT_MS = 3000;
 const PRELOAD_RETRY_DELAY_MS = 50;
 
 const PROBLEM_RETRY_ATTEMPTS = 10;
