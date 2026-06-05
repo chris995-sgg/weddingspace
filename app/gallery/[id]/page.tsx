fe70059dc330195c684a8d89378b235808ec7528
Chris <chriskitscha@gmail.com>
2026-06-05 08:18:20 +0200

    8 20 400

--- app/gallery/[id]/page.tsx
+++ app/gallery/[id]/page.tsx
@@ -41,8 +41,8 @@ type ImageLoadReport = {
 
 const CONCURRENT_LOADS = 8;
 
-const PRELOAD_ATTEMPTS = 30;
-const PRELOAD_TIMEOUT_MS = 500;
+const PRELOAD_ATTEMPTS = 20;
+const PRELOAD_TIMEOUT_MS = 400;
 const PRELOAD_RETRY_DELAY_MS = 50;
 
 const VISIBLE_IMG_RETRIES = 5;
